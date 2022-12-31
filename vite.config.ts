f015cce96285ea822f66554fad912b120f674c71
leigp <907090585@qq.com>
2022-12-31 17:05:51 +0800

    docs: 添加antd组件库

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,15 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import { viteMockServe } from 'vite-plugin-mock'
 import viteCompression from 'vite-plugin-compression'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      resolvers: [AntDesignVueResolver()]
+    }),
     viteMockServe({
       // default
       mockPath: 'mock'
