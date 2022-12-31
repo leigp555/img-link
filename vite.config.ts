06b55e8afbddadbb1f18fcbd79ceb09515e81a8c
leigp <907090585@qq.com>
2022-12-31 17:20:16 +0800

    docs: 添加svg插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // 如果编辑器提示 path 模块找不到，则可以安装一下 @types/node -> npm i @types/node -D
-import { resolve } from 'path'
+import path, { resolve } from 'path'
 import { viteMockServe } from 'vite-plugin-mock'
 import viteCompression from 'vite-plugin-compression'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 export default defineConfig({
   plugins: [
@@ -13,8 +14,14 @@ export default defineConfig({
     Components({
       resolvers: [AntDesignVueResolver()]
     }),
+    createSvgIconsPlugin({
+      // 指定需要缓存的图标文件夹
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/svg')],
+      // 指定symbolId格式
+      symbolId: 'icon-[name]',
+      inject: 'body-first'
+    }),
     viteMockServe({
-      // default
       mockPath: 'mock'
     }),
     viteCompression()
@@ -24,6 +31,7 @@ export default defineConfig({
       '@': resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
     }
   },
+  base: './',
   build: {
     minify: 'terser',
     terserOptions: {
