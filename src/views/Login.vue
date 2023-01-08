0085416183d9f5539a66c96dcd5a02997630dc00
leigp <907090585@qq.com>
2023-01-08 22:19:23 +0800

    feat: 优化动画

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -62,13 +62,14 @@ onMounted(() => {
           autocomplete="on"
           @finish="onFinish"
           @finishFailed="onFinishFailed"
-          class="login-form animate__animated animate__fadeInLeft"
+          class="login-form"
           ref="formRef"
         >
           <!--用户名-->
           <a-form-item
             name="username"
             :rules="[{ required: true, message: 'Please input your username!' }]"
+            class="username animate__animated animate__fadeInLeft"
           >
             <a-input
               v-model:value="formState.username"
@@ -79,6 +80,7 @@ onMounted(() => {
           <!--密码-->
           <a-form-item
             name="password"
+            class="password animate__animated animate__fadeInLeft"
             :rules="[{ required: true, message: 'Please input your password!' }]"
           >
             <a-input-password
@@ -88,14 +90,17 @@ onMounted(() => {
             />
           </a-form-item>
           <!--记住密码/忘记密码-->
-          <a-form-item name="remember">
+          <a-form-item
+            name="remember"
+            class="remember animate__animated animate__fadeInLeft"
+          >
             <div class="action">
               <a-checkbox v-model:checked="formState.remember">记住密码</a-checkbox>
               <router-link to="">忘记密码?</router-link>
             </div>
           </a-form-item>
           <!--登录-->
-          <a-form-item>
+          <a-form-item class="loginButton animate__animated animate__fadeInLeft">
             <a-button
               type="primary"
               html-type="submit"
@@ -103,13 +108,17 @@ onMounted(() => {
               >登录</a-button
             >
           </a-form-item>
-          <a-form-item>
+          <a-form-item class="goRegister animate__animated animate__fadeInLeft">
             <router-link to="/register">没有账号?点击注册</router-link>
           </a-form-item>
         </a-form>
         <!--其他方式登录-->
-        <a-divider style="font-size: 14px">其他登录方式</a-divider>
-        <div class="other-way-login">
+        <a-divider
+          style="font-size: 14px"
+          class="other-way animate__animated animate__fadeInLeft"
+          >其他登录方式</a-divider
+        >
+        <div class="other-way-login animate__animated animate__fadeInLeft">
           <a-button type="text" shape="circle" @click="githubCode">
             <template #icon><github-outlined class="svg" /></template>
           </a-button>
@@ -124,4 +133,26 @@ onMounted(() => {
 
 <style scoped lang="scss">
 @import '../style/login.scss';
+
+.username.animate__animated.animate__fadeInLeft {
+  --animate-duration: 0.5s;
+}
+.password.animate__animated.animate__fadeInLeft {
+  --animate-duration: 0.6s;
+}
+.remember.animate__animated.animate__fadeInLeft {
+  --animate-duration: 0.7s;
+}
+.loginButton.animate__animated.animate__fadeInLeft {
+  --animate-duration: 0.8s;
+}
+.goRegister.animate__animated.animate__fadeInLeft {
+  --animate-duration: 0.9s;
+}
+.other-way.animate__animated.animate__fadeInLeft {
+  --animate-duration: 1s;
+}
+.other-way-login.animate__animated.animate__fadeInLeft {
+  --animate-duration: 1s;
+}
 </style>
