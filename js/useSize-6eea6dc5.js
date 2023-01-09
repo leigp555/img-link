3c2bedbb434bffb4d2175a0783520a4b43345b1c
leigp555 <leigp555@users.noreply.github.com>
2023-01-09 16:13:22 +0000

    publish 9b85d3f00bf4a2581bb95306f936ba895c6839c0

--- js/useSize-6eea6dc5.js
+++
@@ -1,1 +0,0 @@
-import{C as r}from"./default-37a9e99b.js";import{e as n,v as e,y as o}from"./index-44b8c168.js";var t=Symbol("SizeProvider"),i=function(i){var u=e("configProvider",r),a=n((function(){return i.size||u.componentSize}));return o(t,a),a},u=function(r){return r?n((function(){return r.size})):e(t,n((function(){return"default"})))};export{i as a,u};
---
+++ js/useSize-b17d54fa.js
@@ -0,0 +1,1 @@
+import{C as r}from"./default-f04a21e2.js";import{e as n,t as e,x as t}from"./index-ef84d244.js";var o=Symbol("SizeProvider"),i=function(i){var u=e("configProvider",r),a=n((function(){return i.size||u.componentSize}));return t(o,a),a},u=function(r){return r?n((function(){return r.size})):e(o,n((function(){return"default"})))};export{i as a,u};
